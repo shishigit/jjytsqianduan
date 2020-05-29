894b50624b70dc89e5b868165cf592f351bddb78
chendaliang <piaoyaocdl@163.com>
2020-05-29 14:43:40 +0800

    1

--- src/app/page/denglu/denglu.component.ts
+++ src/app/page/denglu/denglu.component.ts
@@ -1,23 +1,18 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
     selector: 'app-denglu',
     templateUrl: './denglu.component.html',
     styleUrls: ['./denglu.component.css']
 })
-export class DengluComponent implements OnInit
+export class DengluComponent
 {
     dengluxinxi = {
         zhanghao: '',
         mima: ''
     }
 
-    constructor()
+    denglu()
     {
     }
-
-    ngOnInit(): void
-    {
-    }
-
 }
