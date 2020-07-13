44799577d21c5b49ccfd59caf53ded8bbab37804
chendaliang <piaoyaocdl@163.com>
2020-07-13 16:08:52 +0800

    1

--- src/app/page/zhuye/zhuye.component.ts
+++ src/app/page/zhuye/zhuye.component.ts
@@ -1,28 +1,77 @@
-import {AfterViewInit, Component, ComponentFactoryResolver, ComponentRef, QueryList, ViewChildren} from '@angular/core';
+import {
+    AfterViewInit,
+    Component,
+    ComponentFactoryResolver,
+    ComponentRef,
+    OnInit,
+    QueryList,
+    ViewChildren
+} from '@angular/core';
 import {HoutaishujuService} from "../../service/houtaishuju.service";
 import {YeqianDirective} from "../../zujian/yeqian.directive";
+import {YonghuguanliComponent} from "../xitongguanli/yonghuguanli/yonghuguanli.component";
+import {JueseguanliComponent} from "../xitongguanli/jueseguanli/jueseguanli.component";
+import {BumenguanliComponent} from "../xitongguanli/bumenguanli/bumenguanli.component";
+
 
 @Component({
     selector: 'app-zhuye',
     templateUrl: './zhuye.component.html',
     styleUrls: ['./zhuye.component.css']
 })
-export class ZhuyeComponent implements AfterViewInit
+export class ZhuyeComponent implements AfterViewInit, OnInit
 {
     @ViewChildren(YeqianDirective) yeqianDirectives: QueryList<YeqianDirective>;
     yeqianshuju: { zujian: ComponentRef<any>, biaoti: string, leixing: any }[] = []
     dangqianyeqian: number;
+    yemianurl: {
+        [key: string]: {
+            [key: string]: {
+                yemian: any,
+                url: string[],
+                xianshi: boolean
+            }
+        }
+    } = {
+        系统管理: {
+            用户管理: {
+                yemian: YonghuguanliComponent,
+                url: [
+                    '/yonghu/tianjia',
+                    '/yonghu/chaxun',
+                    '/yonghu/jihuo',
+                    '/yonghu/shanchu',
+                    '/yonghu/xiugaijuese',
+                    '/yonghu/chaxunjuese',
+                ],
+                xianshi: false
+            },
+            角色管理: {
+                yemian: JueseguanliComponent,
+                url: [
+                    '/juese/chaxun',
+                    '/juese/jihuo',
+                    '/juese/tianjia',
+                    '/juese/xiugai',
+                    '/juese/xiugaijiekou',
+                    '/juese/chaxunjiekou',
+                ],
+                xianshi: false
+            },
+            部门管理: {
+                yemian: BumenguanliComponent,
+                url: ['/bumen/chaxun',],
+                xianshi: false
+            },
+        }
+    };
+    yijiliebiao: string[] = []
 
     constructor(
         readonly houtaishujuService: HoutaishujuService,
         private readonly factoryResolver: ComponentFactoryResolver
     )
     {
-    }
-
-    yijiliebiao()
-    {
-        return Object.keys(this.houtaishujuService.zuocecaidan)
     }
 
     caidandianji(erji: { biaoqian: string, yemian: any })
@@ -54,14 +103,15 @@ export class ZhuyeComponent implements AfterViewInit
         this.dangqianyeqian = this.yeqianshuju.length
     }
 
+
     erjiliebiao(yiji: string)
     {
-        return Object.keys(this.houtaishujuService.zuocecaidan[yiji])
+        return Object.keys(this.yemianurl[yiji])
             .map(value =>
             {
                 return {
                     biaoqian: value,
-                    yemian: this.houtaishujuService.zuocecaidan[yiji][value].yemian
+                    yemian: this.yemianurl[yiji][value].yemian
                 }
             })
     }
@@ -83,11 +133,53 @@ export class ZhuyeComponent implements AfterViewInit
                 }
             })
         })
+
     }
 
     guanbiyeqian(ls: number)
     {
         this.yeqianshuju[ls].zujian.destroy();
         this.yeqianshuju.splice(ls, 1)
+    }
+
+    chulicaidan()
+    {
+        this.houtaishujuService.yongyoujiekous
+            .filter(value => value.jianquan === 'jianquan')
+            .map(value => value.url)
+            .forEach(value =>
+            {
+                for (let yijikey in this.yemianurl)
+                {
+                    for (let erjikey in this.yemianurl[yijikey])
+                    {
+                        if (!this.yemianurl[yijikey].hasOwnProperty(erjikey)) continue;
+                        let ls = this.yemianurl[yijikey][erjikey]
+                        if (ls.url.includes(value)) ls.xianshi = true
+                    }
+                }
+            })
+
+        for (let yijikey in this.yemianurl)
+        {
+            for (let erjikey in this.yemianurl[yijikey])
+            {
+                if (!this.yemianurl[yijikey].hasOwnProperty(erjikey)) continue;
+                if (!this.yemianurl[yijikey][erjikey].xianshi) delete this.yemianurl[yijikey][erjikey]
+            }
+
+            if (Object.keys(this.yemianurl[yijikey]).length === 0)
+            {
+                delete this.yemianurl[yijikey]
+            }
+        }
+
+        this.yijiliebiao = Object.keys(this.yemianurl)
+
+    }
+
+    ngOnInit(): void
+    {
+        this.chulicaidan()
     }
 }
--- src/app/service/houtaishuju.service.ts
+++ src/app/service/houtaishuju.service.ts
@@ -1,100 +1,15 @@
 import {Injectable} from '@angular/core';
 import {http_xitong} from "./http.jiekou";
-import {YonghuguanliComponent} from "../page/xitongguanli/yonghuguanli/yonghuguanli.component";
-import {JueseguanliComponent} from "../page/xitongguanli/jueseguanli/jueseguanli.component";
-import {BumenguanliComponent} from "../page/xitongguanli/bumenguanli/bumenguanli.component";
 import dengluRes = http_xitong.dengluRes;
 
-const sessionStorageKey = {
-    yongyoujiekous: 'yongyoujiekous'
-}
-
-const yemianurl: {
-    [key: string]: {
-        [key: string]: {
-            yemian: any,
-            url: string[],
-            xianshi: boolean
-        }
-    }
-} = {
-    系统管理: {
-        用户管理: {
-            yemian: YonghuguanliComponent,
-            url: [
-                '/yonghu/tianjia',
-                '/yonghu/chaxun',
-                '/yonghu/jihuo',
-                '/yonghu/shanchu',
-                '/yonghu/xiugaijuese',
-                '/yonghu/chaxunjuese',
-            ],
-            xianshi: false
-        },
-        角色管理: {
-            yemian: JueseguanliComponent,
-            url: [
-                '/juese/chaxun',
-                '/juese/jihuo',
-                '/juese/tianjia',
-                '/juese/xiugai',
-                '/juese/xiugaijiekou',
-                '/juese/chaxunjiekou',
-            ],
-            xianshi: false
-        },
-        部门管理: {
-            yemian: BumenguanliComponent,
-            url: ['/bumen/chaxun',],
-            xianshi: false
-        },
-    }
-};
+const sessionStorageKey = {}
 
 @Injectable({
     providedIn: 'root'
 })
 export class HoutaishujuService
 {
-    set yongyoujiekous(ls: Array<dengluRes>)
-    {
-        sessionStorage.setItem(sessionStorageKey.yongyoujiekous, JSON.stringify(ls))
-    }
-
-    get zuocecaidan()
-    {
-        (JSON.parse(sessionStorage.getItem(sessionStorageKey.yongyoujiekous)) as Array<dengluRes>)
-            .filter(value => value.jianquan === 'jianquan')
-            .map(value => value.url)
-            .forEach(value =>
-            {
-                for (let yijikey in yemianurl)
-                {
-                    for (let erjikey in yemianurl[yijikey])
-                    {
-                        if (!yemianurl[yijikey].hasOwnProperty(erjikey)) continue;
-                        let ls = yemianurl[yijikey][erjikey]
-                        if (ls.url.includes(value)) ls.xianshi = true
-                    }
-                }
-            })
-
-        for (let yijikey in yemianurl)
-        {
-            for (let erjikey in yemianurl[yijikey])
-            {
-                if (!yemianurl[yijikey].hasOwnProperty(erjikey)) continue;
-                if (!yemianurl[yijikey][erjikey].xianshi) delete yemianurl[yijikey][erjikey]
-            }
-
-            if (Object.keys(yemianurl[yijikey]).length === 0)
-            {
-                delete yemianurl[yijikey]
-            }
-        }
-
-        return yemianurl;
-    }
+    yongyoujiekous: Array<dengluRes> = [];
 
     constructor()
     {
