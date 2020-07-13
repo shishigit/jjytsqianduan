08ecc711ae228cd62e7aeaca9ab14e17bb205820
chendaliang <piaoyaocdl@163.com>
2020-07-13 16:53:35 +0800

    1

--- src/app/service/houtaishuju.service.ts
+++ src/app/service/houtaishuju.service.ts
@@ -1,16 +1,10 @@
 import {Injectable} from '@angular/core';
-import {http_xitong} from "./http.jiekou";
-import dengluRes = http_xitong.dengluRes;
-
-const sessionStorageKey = {}
 
 @Injectable({
     providedIn: 'root'
 })
 export class HoutaishujuService
 {
-    yongyoujiekous: Array<dengluRes> = [];
-
     constructor()
     {
     }
